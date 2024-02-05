12a29fe30a6d09f879909d95649b1449c47d5261
SufiaRashid <sufiakulthum@email.com>
2024-02-05 14:16:13 -0500

    added

--- website/views.py
+++ website/views.py
@@ -12,3 +12,43 @@ def home():
     current_user.logoutPage = request.url
     db.session.commit()
     return render_template("home.html", user=current_user)
+
+@views.route('/leaderboard')
+#login required to access this page
+@login_required
+def leaderboard():
+    #allows for user to end up on black and white page when they log in if they log out on home page
+    current_user.logoutPage = request.url
+    
+    db.session.commit()
+    return render_template("leaderBoard.html", user=current_user)
+
+@views.route('/account')
+#login required to access this page
+@login_required
+def account():
+    #allows for user to end up on black and white page when they log in if they log out on home page
+    current_user.logoutPage = request.url
+    
+    db.session.commit()
+    return render_template("account.html", user=current_user)
+
+@views.route('/settings')
+#login required to access this page
+@login_required
+def settings():
+    #allows for user to end up on black and white page when they log in if they log out on home page
+    current_user.logoutPage = request.url
+    
+    db.session.commit()
+    return render_template("settings.html", user=current_user)
+
+@views.route('/support')
+#login required to access this page
+@login_required
+def support():
+    #allows for user to end up on black and white page when they log in if they log out on home page
+    current_user.logoutPage = request.url
+    
+    db.session.commit()
+    return render_template("support.html", user=current_user)
