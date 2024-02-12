c9288f2bbffa5c7e605a873c08e1cc2a489a638d
SufiaRashid <sufiakulthum@email.com>
2024-02-12 10:31:31 -0500

    imported bootstrap

--- README.md
+++ README.md
@@ -4,19 +4,26 @@ A sports prediction web app for tracking and comparing your predictions with fri
 
 # Instructions
 
-## Step One - Prerequisites: 
-- Install Node.js - https://nodejs.org/en/download
-- Clone the repository: ```git clone https://github.com/SufiaRashid/Flame-Picks.git```
-## Step Two - Package requirements:
-- Navigate to the root directory of the repository: ```cd Flame-Picks```
-- Install from requirements.txt: ```pip install -r requirements.txt```
-- Navigate to client folder: ```cd client```
-- Install packages: ```npm install```
-## Step Three - Running application:
-- Navigate back to root: ```cd ..```
-- Run start.sh to run application: ```./start.sh``` or ```sh start.sh``` depending on the environment
-## Issues
-**Failed permission to run script**
-- Make sure permission is given to run start.sh: ```chmod +x start.sh```
-- If the above doesn't work, open a PowerShell terminal in admin mode and run:<br> ```Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser```
+## Step One - Prerequisites:
 
+- Install Node.js - https://nodejs.org/en/download
+- Clone the repository: `git clone https://github.com/SufiaRashid/Flame-Picks.git`
+
+## Step Two - Package requirements:
+
+- Navigate to the root directory of the repository: `cd Flame-Picks`
+- Install from requirements.txt: `pip install -r requirements.txt`
+- Navigate to client folder: `cd client`
+- Install packages: `npm install`
+
+## Step Three - Running application:
+
+- Navigate back to root: `cd ..`
+- Run start.sh to run application: `./start.sh` or `sh start.sh` depending on the environment
+
+## Issues
+
+**Failed permission to run script**
+
+- Make sure permission is given to run start.sh: `chmod +x start.sh`
+- If the above doesn't work, open a PowerShell terminal in admin mode and run:<br> `Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser`
--- client/src/components/BaseLayout.js
+++ client/src/components/BaseLayout.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import "https://code.jquery.com/jquery-3.5.1.slim.min.js";
+import "https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js";
+import "https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js";
 
 const BaseLayout = ({ children }) => {
   const { authData, logout } = useAuth();
@@ -10,46 +13,77 @@ const BaseLayout = ({ children }) => {
 
   const handleLogout = () => {
     logout();
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light sticky-top" style={{ backgroundColor: '#afd7d4' }}>
+      <nav
+        className="navbar navbar-expand-lg navbar-light sticky-top"
+        style={{ backgroundColor: "#afd7d4" }}
+      >
         <div className="container-fluid">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav me-auto mb-2 mb-lg-0">
               {authData.isAuthenticated ? (
                 <>
-                  <Link className="nav-item nav-link" to="/home"><i className="fa fa-fw fa-home"></i> Make Picks</Link>
-                  <Link className="nav-item nav-link" to="/leaderboard"><i className="fa fa-fw fa-list"></i> LeaderBoard</Link>
-                  <Link className="nav-item nav-link" to="/account"><i className="fa fa-fw fa-user"></i> Account</Link>
-                  <Link className="nav-item nav-link" to="/settings"><i className="fa fa-fw fa-cogs"></i> Settings</Link>
-                  <Link className="nav-item nav-link" to="/support"><i className="fa fa-fw fa-wrench"></i> Support</Link>
+                  <Link className="nav-item nav-link" to="/home">
+                    <i className="fa fa-fw fa-home"></i> Make Picks
+                  </Link>
+                  <Link className="nav-item nav-link" to="/leaderboard">
+                    <i className="fa fa-fw fa-list"></i> LeaderBoard
+                  </Link>
+                  <Link className="nav-item nav-link" to="/account">
+                    <i className="fa fa-fw fa-user"></i> Account
+                  </Link>
+                  <Link className="nav-item nav-link" to="/settings">
+                    <i className="fa fa-fw fa-cogs"></i> Settings
+                  </Link>
+                  <Link className="nav-item nav-link" to="/support">
+                    <i className="fa fa-fw fa-wrench"></i> Support
+                  </Link>
                 </>
               ) : (
                 <>
-                  <Link className="nav-item nav-link" to="/login"><i className="fa fa-fw fa-sign-in"></i> Login</Link>
-                  <Link className="nav-item nav-link" to="/sign-up"><i className="fa fa-fw fa-user-o"></i> Sign-Up</Link>
+                  <Link className="nav-item nav-link" to="/login">
+                    <i className="fa fa-fw fa-sign-in"></i> Login
+                  </Link>
+                  <Link className="nav-item nav-link" to="/sign-up">
+                    <i className="fa fa-fw fa-user-o"></i> Sign-Up
+                  </Link>
                 </>
               )}
             </div>
             {authData.isAuthenticated && (
               <div className="navbar-nav ms-auto">
-                <span className="navbar-text me-3">{authData.user?.firstName} {authData.user?.lastName}</span>
-                <button className="nav-link btn btn-clear" onClick={handleLogout} style={{ boxShadow: 'none' }}><i className="fa fa-fw fa-sign-out"></i> Logout</button>
+                <span className="navbar-text me-3">
+                  {authData.user?.firstName} {authData.user?.lastName}
+                </span>
+                <button
+                  className="nav-link btn btn-clear"
+                  onClick={handleLogout}
+                  style={{ boxShadow: "none" }}
+                >
+                  <i className="fa fa-fw fa-sign-out"></i> Logout
+                </button>
               </div>
             )}
           </div>
         </div>
       </nav>
 
-      <div className="container">
-        {children}
-      </div>
+      <div className="container">{children}</div>
     </>
   );
 };
