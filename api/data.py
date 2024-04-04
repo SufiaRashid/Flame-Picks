55b8a34eccf9a71efda1c4fb683c2f993eb784c3
campisir <rc113@students.uwf.edu>
2024-04-04 11:20:41 -0400

    more data retrieval + testing

--- api/data.py
+++ api/data.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, jsonify
-from api.models import Game, GamePick
+from datetime import datetime
+from flask import Blueprint, jsonify, request
+from . import db
+from api.models import Game, GamePick, User
 
 
 data = Blueprint('data', __name__)
@@ -32,3 +34,64 @@ def get_all_gamepicks():
     } for pick in gamepicks]
     
     return jsonify(gamepicks_list), 200
+
+@data.route('/get-users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    users_list = [{
+        'id': user.id,
+        'email': user.email,
+        'firstName': user.firstName,
+        'lastName': user.lastName
+    } for user in users]
+
+    return jsonify(users_list), 200
+
+#USED FOR TESTING ONLY
+@data.route('/post-user', methods=['POST'])
+def post_user():
+    data = request.get_json()
+    new_user = User(
+        email=data['email'],
+        password=data['password'],
+        firstName=data['firstName'],
+        lastName=data['lastName']
+    )
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User created successfully'}), 201
+
+#USED FOR TESTING ONLY
+@data.route('/post-gamepick', methods=['POST'])
+def post_gamepick():
+    data = request.get_json()
+    new_gamepick = GamePick(
+        user_id=data['user_id'],
+        game_id=data['game_id'],
+        picked_team=data['picked_team'],
+        pick_time=datetime.utcnow(),
+        result=data.get('result')
+    )
+    db.session.add(new_gamepick)
+    db.session.commit()
+
+    return jsonify({'message': 'GamePick created successfully'}), 201
+
+#USED FOR TESTING ONLY
+@data.route('/post-game', methods=['POST'])
+def post_game():
+    data = request.get_json()
+    new_game = Game(
+        game_id=data['game_id'],
+        home_team=data['home_team'],
+        away_team=data['away_team'],
+        date=data['date'],
+        score=data.get('score', '-'),
+        time=data.get('time', '1:00pm'),
+        winning_team=data.get('winning_team') 
+    )
+    db.session.add(new_game)
+    db.session.commit()
+
+    return jsonify({'message': 'Game created successfully'}), 201
